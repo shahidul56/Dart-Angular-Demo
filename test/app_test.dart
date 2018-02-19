d98db17b9332d109b9e971e795c52c245c7d694e
ing.brayan.martinez <Brayan_martinez000@hotmail.com>
2018-02-19 14:28:32 -0400

    cambios

---
+++ test/app_test.dart
@@ -0,0 +1,27 @@
+
+
+@AngularEntrypoint()
+void main() {
+  final testBed = new NgTestBed<AppComponent>();
+  NgTestFixture<AppComponent> fixture;
+
+  setUp(() async {
+    fixture = await testBed.create();
+  });
+
+  tearDown(disposeAnyRunningTest);
+
+  test('Default greeting', () {
+    expect(fixture.text, 'Hello Angular');
+  });
+
+  test('Greet world', () async {
+    await fixture.update((c) => c.name = 'World');
+    expect(fixture.text, 'Hello World');
+  });
+
+  test('Greet world HTML', () {
+    final html = fixture.rootElement.innerHtml;
+    expect(html, '<h1>Hello Angular</h1>');
+  });
+}
---
+++ web/component/app_component.dart
@@ -0,0 +1,9 @@
+
+
+@Component(
+  selector: 'my-app',
+  templateUrl: 'app_component.html',
+)
+class AppComponent {
+  var name = 'Angular';
+}
